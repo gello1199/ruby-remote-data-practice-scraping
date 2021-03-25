d5f5a33f567e5fa107cc939ec34c56c8d20b07ca
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2021-03-25 14:59:11 -0400

    commit tree 5d6be102e889c9317b02d53d0139efb639923f6f

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,26 @@
+# require 'nokogiri'
+# require 'open-uri'
+
+# # html = open("https://flatironschool.com/")
+
+# # doc = Nokogiri::HTML(html)
+
+# doc = Nokogiri::HTML(open("http://flatironschool.com/")) #refactored line 4 and 6
+# doc.css(".site-header__hero__headline")
+
+# puts doc.css(".site-header__hero__headline").text
+
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+html = open("http://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+
+doc.css(".tout__label.heading.heading--level-4") 
+
+courses = doc.css(".tout__label.heading.heading--level-4")
+
+courses.each do |course|
+  puts course.text.strip
+end 
+
